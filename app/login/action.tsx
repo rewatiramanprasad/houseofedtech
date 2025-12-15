5502411bc8215c19c9a0f2167b2ceed65a0ddfd6
Raman Prasad <ramanprasad.0203@gmail.com>
2025-12-15 11:36:17 +0530

    feat: api added to login -jwt deps added

---
+++ app/login/action.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { LoginFormSchemaType } from './page'
+import { prisma } from '@/lib/prisma'
+import { cookies } from 'next/headers'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+
+type LoginResponse = { success: true } | { success: false; message: string }
+export default async function loginAction(
+  input: LoginFormSchemaType
+): Promise<LoginResponse> {
+  try {
+    const { email, password } = input
+    const user = await prisma.user.findUnique({ where: { email } })
+    if (!user) {
+      return { success: false, message: 'Invalid email or password' }
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return { success: false, message: 'Invalid email or password' }
+    }
+    const token: string = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '7d' }
+    )
+
+    const cookieStore = await cookies()
+    cookieStore.set('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+
+    return { success: true }
+  } catch (error) {
+    console.error('Login error:', error)
+
+    return {
+      success: false,
+      message: 'Something went wrong. Please try again.',
+    }
+  }
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,43 @@
 'use client'
 import Link from 'next/link'
 import React, { useState } from 'react'
+import { z } from 'zod'
+import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import FormInput from '@/components/formInput'
+import { Form } from '@/components/ui/form'
+import loginAction from './action'
 
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  password: z.string().min(2, {
+    message: 'Password must be at least 2 characters.',
+  }),
+})
+export type LoginFormSchemaType = z.infer<typeof formSchema>
 
 export default function LoginForm() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleSubmit = () => {
-    console.log('Login submitted:', { email, password })
+  const router = useRouter()
+  const form = useForm<LoginFormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+  async function onSubmit(values: LoginFormSchemaType) {
+    const result = await loginAction(values)
+    form.reset()
+    if (result.success) {
+      toast.success('Logged in successfully')
+      router.push('/dashboard')
+    } else {
+      toast.error(result.message)
+    }
   }
 
   return (
@@ -26,50 +55,68 @@ export default function LoginForm() {
             Enter your details to access your account
           </p>
         </div>
-
-        <div className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700 mb-2"
-            >
-              Email address
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="you@example.com"
-              className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
-            />
-          </div>
-
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700 mb-2"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="••••••••"
-              className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
-            />
-          </div>
-
-          <button
-            onClick={handleSubmit}
-            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 rounded-lg transition-colors duration-200"
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            onReset={() => form.reset()}
+            className="space-y-8"
           >
-            Log in
-          </button>
-        </div>
+            <div className="space-y-6">
+              <div>
+                {/* <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  Email address
+                </label> */}
+                {/* <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="you@example.com"
+                  className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
+                /> */}
+                <FormInput<LoginFormSchemaType>
+                  form={form}
+                  fieldName={'email'}
+                  placeHolder="Email..."
+                  formType="email"
+                />
+              </div>
 
+              <div>
+                {/* <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  Password
+                </label> */}
+                {/* <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="••••••••"
+                  className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
+                /> */}
+                <FormInput<LoginFormSchemaType>
+                  form={form}
+                  fieldName={'password'}
+                  placeHolder="••••••••"
+                  formType="password"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 rounded-lg transition-colors duration-200"
+              >
+                Log in
+              </button>
+            </div>
+          </form>
+        </Form>
         <div className="text-center mt-6">
           <p className="text-gray-400 text-sm">
             Don't have an account?{' '}
